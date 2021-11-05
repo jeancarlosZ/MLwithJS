be078b9c0bc9bab302a85a29c8c9dc6158e4d471
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-11-05 14:27:55 -0400

    feat: runAnalysis function can now calculate prediction accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -7,8 +6,21 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  let numberCorrect = 0;
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
+  }
+  console.log("Accuracy: ", numberCorrect / testSetSize);
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -18,12 +30,10 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log("Your point will probably fall into", bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
 }
 
 function splitDataset(data, testCount) {
