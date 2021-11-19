44613bd7f3acdf5971926863957a427a93565cda
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-11-18 23:06:14 -0500

    feat: gradientDescent method can calculate current guesses for MPG

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -6,6 +6,15 @@ class LinearRegression {
     this.labels = labels;
 
     this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
+
+    this.m = 0;
+    this.b = 0;
+  }
+
+  gradientDescent() {
+    const currentGuessesForMPG = this.features.map(row => {
+      return this.m * row[0] + this.b;
+    });
   }
 
   train() {
