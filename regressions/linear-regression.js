f48c9df234aaa0ac622ec707a6da65cf48fc6e60
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-12-04 22:51:27 -0500

    feat: train method can run on batches of data to improve performance with fewer iterations

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -26,7 +26,12 @@ class LinearRegression {
 
     for (let i = 0; i < this.options.iterations; i++) {
       for (let j = 0; j < batchQuantity; j++) {
-        this.gradientDescent();
+        const { batchSize } = this.options;
+        const startIndex = j * batchSize;
+        const featureSlice = this.features.slice([startIndex, 0], [batchSize, -1]);
+        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+
+        this.gradientDescent(featureSlice, labelSlice);
       }
 
       this.recordMSE();
