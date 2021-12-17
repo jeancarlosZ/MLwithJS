c6b9b64a725aeaa63ea72b86fa36d23d40414422
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-12-17 17:15:53 -0500

    refactor: can plot the costHistory

--- regressions/logistic-regression/index.js
+++ regressions/logistic-regression/index.js
@@ -2,6 +2,7 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("../load-csv");
 const LogisticRegression = require("./logistic-regression");
+const plot = require("node-remote-plot");
 
 const { features, labels, testFeatures, testLabels } = loadCSV("../data/cars.csv", {
   dataColumns: ["horsepower", "displacement", "weight"],
@@ -18,10 +19,14 @@ const { features, labels, testFeatures, testLabels } = loadCSV("../data/cars.csv
 const regression = new LogisticRegression(features, labels, {
   learningRate: 0.5,
   iterations: 100,
-  batchSize: 50,
+  batchSize: 10,
   decisionBoundary: 0.5,
 });
 
 regression.train();
 
 console.log(regression.test(testFeatures, testLabels));
+
+plot({
+  x: regression.costHistory.reverse(),
+});
