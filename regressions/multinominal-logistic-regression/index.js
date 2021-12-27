f2749dfde8275306e71c994079599dc64f965519
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-12-26 22:21:01 -0500

    refactor: training multinomial model

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -3,6 +3,7 @@ const tf = require("@tensorflow/tfjs");
 const loadCSV = require("../load-csv");
 const LogisticRegression = require("./logistic-regression");
 const plot = require("node-remote-plot");
+const _ = require("lodash");
 
 const { features, labels, testFeatures, testLabels } = loadCSV("../data/cars.csv", {
   dataColumns: ["horsepower", "displacement", "weight"],
@@ -24,19 +25,14 @@ const { features, labels, testFeatures, testLabels } = loadCSV("../data/cars.csv
   },
 });
 
-console.log(labels);
+const regression = new LogisticRegression(features, _.flatMap(labels), {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 10,
+});
 
-// const regression = new LogisticRegression(features, labels, {
-//   learningRate: 0.5,
-//   iterations: 100,
-//   batchSize: 10,
-//   decisionBoundary: 0.5,
-// });
+regression.weights.print();
 
-// regression.train();
+regression.train();
 
-// console.log(regression.test(testFeatures, testLabels));
-
-// plot({
-//   x: regression.costHistory.reverse(),
-// });
+regression.predict([[215, 440, 2.16]]).print();
